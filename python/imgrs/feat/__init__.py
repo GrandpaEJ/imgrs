8b9d7d51e776eaab37c26ded10cca797472d3a9b
Grandpa EJ <abirmridha2005@gmail.com>
2025-11-27 11:11:54 +0600

    Remove old text mixin files from feat directory

--- python/imgrs/feat/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Feature modules for advanced image processing capabilities.
-"""
-
-from .text import TextMixin
-
-__all__ = ['TextMixin']
--- python/imgrs/feat/text/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Advanced text rendering features for imgrs.
-"""
-
-from .text_mixin import TextMixin
-
-__all__ = ['TextMixin']
