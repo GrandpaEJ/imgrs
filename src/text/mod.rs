231f5e0a5dc8b7cb1b58eeac9596f48848f09bb2
Grandpa EJ <abirmridha2005@gmail.com>
2025-11-27 12:01:27 +0600

    Fix Rust code with cargo fix

--- src/text/mod.rs
+++ src/text/mod.rs
@@ -16,7 +16,6 @@ pub use renderer::{
     draw_text, draw_text_multiline, draw_text_styled, draw_text_centered,
     get_text_size, get_multiline_text_size, get_text_box
 };
-pub use styles::{TextStyle, TextAlign};
 // Additional text functions available for future use
 // pub use renderer::{draw_text_quick, TextBox};
 // pub use styles::FontWeight;
